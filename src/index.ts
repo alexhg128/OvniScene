8f0d7ac90c2d2408489a1391d0b659ce965790a3
Alejandro Hahn <halexhg@gmail.com>
2021-01-28 19:33:11 -0700

    Three js init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-alert("Hello World!!");
+import * as THREE from 'three';
+
+let camera: THREE.Camera, scene: THREE.Scene, renderer: any;
+let geometry: any, material: THREE.Material, mesh: THREE.Mesh;
+
+init();
+
+function init() {
+
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setAnimationLoop( animation );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animation(time: any) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
